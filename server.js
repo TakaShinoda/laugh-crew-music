3e3c57cbeaba04d2a5aef331379ec06eacc5321d
Takahiro <tkhr_621@docomo.ne.jp>
2021-05-23 22:14:28 +0900

    test

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = protect(
     safeCompare(username, process.env.USERNAME || 'admin') &&
     safeCompare(password, process.env.PASSWORD || 'admin'),
   {
-    directory: `${__dirname}/public`,
+    directory: `${__dirname}/out`,
     onAuthFailed: (res) => {
       res.end('Authentication failed');
     },
