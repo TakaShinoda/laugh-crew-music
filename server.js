a83f0bf3bb147008ba6a6cc9b93e45a3de34c562
Takahiro <tkhr_621@docomo.ne.jp>
2021-05-23 22:34:07 +0900

    fix auth

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const protect = require('static-auth');
 const safeCompare = require('safe-compare');
 
 const USER_NAME = process.env.USER_NAME || 'admin'; // プロジェクトの環境変数を設定していた場合はそちらを適用させる
-const PASSWORD = process.env.PASSWORD || 'admin';
+const PASSWORD = process.env.PASSWORD || '0516';
 
 const server = protect(
   '/',
